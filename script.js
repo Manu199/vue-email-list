08a48f8ad51056cac52105470364c13a06cdd025
Manu199 <manu199gh@gmail.com>
2023-10-08 18:53:49 +0200

    [add] emails using Axios

--- script.js
+++ script.js
@@ -4,19 +4,20 @@ createApp({
     data(){
         return{
             titolo: 'Ciao Axios!',
-            risultatoChiamataAxios: []
+            emails: [],
         }
     },
     mounted(){
+        
         for (let i = 0; i < 10; i++) {
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then((risposta) => {
                 console.log(risposta.data.response)
-                this.risultatoChiamataAxios.push(risposta.data.response);
+                this.emails.push(risposta.data.response);
             })
             .catch((error) => {
                 console.log(errore.code);
-                this.risultatoChiamataAxios = 'Errore: ' + errore.code;
+                this.emails = 'Errore: ' + errore.code;
             });
         }
     }
